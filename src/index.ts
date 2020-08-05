15ba783f52d15ef554d7519fe241078e51d86606
Hiroki Iwasaki <hiroki.iwasaki.j6c@jp.denso.com>
2020-08-05 10:15:45 +0900

    Added validation rules for irregular input cases [#7]
    Co-authored-by: Philip Hamoui <philippos86@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
 
+if (process.argv[2] === undefined) {
+    console.log('The input cannot be empty. Please specify comma seperated list of candidates');
+    process.exit(1);
+}
+
+if (process.argv.length !== 3) {
+    console.log('Please specify only one list of candidates');
+    process.exit(1);
+}
+
 const membersList: string[] = process.argv[2].split(',');
 
 const randomNumber = (max: number): number => {
     return Math.floor(Math.random() * max);
 };
+
+if (membersList.length === 1) {
+    console.log('Please insert at least 2 participants');
+    process.exit(1);
+}
 
 const memberID = randomNumber(membersList.length);
 console.log('Next is ', membersList[memberID]);
