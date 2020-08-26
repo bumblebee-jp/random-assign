bfd15af82a50bad0b5561b8697a0ac0e880699bc
Hiroki Iwasaki <hiroki.iwasaki.j6c@jp.denso.com>
2020-08-26 11:09:07 +0900

    Add function to check preconditon [#8] - 二人以上入力されていること - 選ばれる人数が1以上の整数であること
    Co-authored-by: Philip Hamoui <philippos86@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
+const printUsage = (): void => {
+    console.log('Usage: next name1,name2,... [assignee number]');
+};
+
+const randomNumber = (max: number): number => {
+    return Math.floor(Math.random() * max);
+};
+
 if (process.argv[2] === undefined) {
     console.log('The input cannot be empty. Please specify comma seperated list of candidates');
     process.exit(1);
@@ -12,12 +20,21 @@ if (process.argv.length !== 3) {
 
 const membersList: string[] = process.argv[2].split(',');
 
-const randomNumber = (max: number): number => {
-    return Math.floor(Math.random() * max);
-};
-
 if (membersList.length === 1) {
-    console.log('Please insert at least 2 participants');
+    printUsage();
+    process.exit(1);
+}
+
+let assigneeNumber: number;
+
+if (process.argv[3] === undefined) {
+    assigneeNumber = 1;
+} else {
+    assigneeNumber = Number(process.argv[3]);
+}
+
+if (!Number.isInteger(assigneeNumber) || assigneeNumber < 1) {
+    printUsage();
     process.exit(1);
 }
 
