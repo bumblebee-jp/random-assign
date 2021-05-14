5157ba1081d4217588f61f61762f5f362e4bb1ca
Hiroki Iwasaki <hiroki.iwasaki.j6c@jp.denso.com>
2021-05-14 18:59:10 +0900

    Update test code to be failed

--- src/__tests__/integration.test.ts
+++ src/__tests__/integration.test.ts
@@ -8,6 +8,5 @@ describe('Happy path test', () => {
 
     it('should be selected people based on second argument', () => {
         expect('Hello').toEqual('Good Bye');
-        expect('aaa').toEqual('bbb');
     });
 });
